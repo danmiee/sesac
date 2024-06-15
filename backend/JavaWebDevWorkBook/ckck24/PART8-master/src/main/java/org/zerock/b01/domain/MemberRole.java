fdf5923e125708435d44038a17ebc25be1b8d5d7
dan-mi <dan-mi@kakao.com>
2024-06-15 09:14:12 +0900

    Remove Sample project

--- backend/JavaWebDevWorkBook/ckck24/PART8-master/src/main/java/org/zerock/b01/domain/MemberRole.java
+++
@@ -1,7 +0,0 @@
-package org.zerock.b01.domain;
-
-public enum MemberRole {
-
-    USER, ADMIN;
-
-}
--- backend/JavaWebDevWorkBook/ckck24/PART9-master/src/main/java/org/zerock/api01/repository/APIUserRepository.java
+++
@@ -1,7 +0,0 @@
-package org.zerock.api01.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.zerock.api01.domain.APIUser;
-
-public interface APIUserRepository extends JpaRepository<APIUser, String> {
-}
