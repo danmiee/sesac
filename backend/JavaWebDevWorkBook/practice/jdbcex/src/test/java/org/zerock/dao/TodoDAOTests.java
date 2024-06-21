50bfe951702e5a91e70ffe280b5adec19834022e
danmiee <dan-mi@kakao.com>
2024-06-21 14:16:27 +0900

    Chapter2: JDBC 실습

---
+++ backend/JavaWebDevWorkBook/practice/jdbcex/src/test/java/org/zerock/dao/TodoDAOTests.java
@@ -0,0 +1,70 @@
+package org.zerock.dao;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.zerock.jdbcex.dao.TodoDAO;
+import org.zerock.jdbcex.domain.TodoVO;
+
+import java.time.LocalDate;
+import java.util.List;
+
+public class TodoDAOTests {
+
+    private TodoDAO todoDAO;
+
+    @BeforeEach
+    public void ready() {
+        todoDAO = new TodoDAO();
+    }
+
+    @Test
+    public void testTime() throws Exception {
+        System.out.println(1);
+        System.out.println(todoDAO.getTime());
+
+        System.out.println(2);
+        System.out.println(todoDAO.getTime2());
+    }
+
+    @Test
+    public void testInsert() throws Exception {
+        TodoVO todoVO = TodoVO.builder()
+                .title("Sample Title...")
+                .dueDate(LocalDate.of(2021, 12, 31))
+                .build();
+
+        todoDAO.insert(todoVO);
+    }
+
+    @Test
+    public void testList() throws Exception {
+        List<TodoVO> list = todoDAO.seleectAll();
+        list.forEach(vo -> System.out.println(vo));
+    }
+
+    @Test
+    public void testSelectOne() throws Exception {
+        Long tno = 1L;
+        TodoVO vo = todoDAO.selectOne(tno);
+        System.out.println(vo);
+    }
+
+    @Test
+    public void testDelete() throws Exception {
+        Long tno = 3L;
+        TodoVO vo = todoDAO.selectOne(tno);
+        todoDAO.deleteOne(tno);
+        System.out.println(vo);
+    }
+
+    @Test
+    public void testUpdate() throws Exception {
+        TodoVO vo = TodoVO.builder()
+                .tno(1L)
+                .title("Updated Title")
+                .dueDate(LocalDate.of(2021, 12, 31))
+                .finished(true)
+                .build();
+        todoDAO.updateOne(vo);
+    }
+}
