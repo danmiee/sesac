00e352726a8f4f158c9fc2638faac06b31e42336
danmiee <danmiee@github.com>
2024-06-29 17:07:43 +0900

    Test for findBySubjectAndContent
    - create vars

--- backend/JumpToSpring/sbb/src/test/java/com/mysite/sbb/SbbApplicationTests.java
+++ backend/JumpToSpring/sbb/src/test/java/com/mysite/sbb/SbbApplicationTests.java
@@ -15,17 +15,22 @@ class SbbApplicationTests {
 	@Test
 	void testJpa() {
 
+		String q1SubjectString = "sbb가 무엇인가요?";
+		String q1ContenString = "sbb에 대해 알고싶어요.";
+		String q2SubString = "스프링부트 모델 질문입니다.";
+		String q2ContentString = "id는 자동으로 생성되나요?";
+
 //		// 질문 데이터 저장
 //		Question q1 = new Question();
-//		q1.setSubject("sbb가 무엇인가요?");
-//		q1.setContent("sbb에 대해 알고싶어요.");
+//		q1.setSubject(q1SubjectString);
+//		q1.setContent(q1ContenString);
 //		q1.setCreateDate(LocalDateTime.now());
 //		// 첫번째 질문 저장
 //		this.questionRepository.save(q1);
 //
 //		Question q2 = new Question();
-//		q2.setSubject("스프링부트 모델 질문입니다.");
-//		q2.setContent("id는 자동으로 생성되나요?");
+//		q2.setSubject(q2SubString);
+//		q2.setContent(q2ContentString);
 //		q2.setCreateDate(LocalDateTime.now());
 //		// 두번째 질문 저장
 //		this.questionRepository.save(q2);
@@ -35,18 +40,21 @@ class SbbApplicationTests {
 //		assertEquals(2, all.size());
 //
 //		Question q = all.get(0);
-//		assertEquals("sbb가 무엇인가요?", q.getSubject());
+//		assertEquals(q1SubjectString, q.getSubject());
 
 //		// findById
 //		Optional<Question> oq = this.questionRepository.findById(1);
 //		if (oq.isPresent()) {
 //			Question q = oq.get();
-//			assertEquals("sbb가 무엇인가요?", q.getSubject());
+//			assertEquals(q1SubjectString, q.getSubject());
 //		}
 
-		// findBySubject
-		Question q = this.questionRepository.findBySubject("sbb가 무엇인가요?");
-		assertEquals(1, q.getId());
+//		// findBySubject
+//		Question q = this.questionRepository.findBySubject(q1SubjectString);
+//		assertEquals(1, q.getId());
 
+		// findBySubjectAndContent
+		Question q = this.questionRepository.findBySubjectAndContent(q1SubjectString, q1ContenString);
+		assertEquals(1, q.getId());
 	}
 }
