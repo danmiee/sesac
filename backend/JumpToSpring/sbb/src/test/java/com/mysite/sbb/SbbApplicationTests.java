0a32c032173f136ae42aaecc07105e24a4a5e36c
danmiee <danmiee@github.com>
2024-06-29 16:36:17 +0900

    Test for findAll from QUESTION table

--- backend/JumpToSpring/sbb/src/test/java/com/mysite/sbb/SbbApplicationTests.java
+++ backend/JumpToSpring/sbb/src/test/java/com/mysite/sbb/SbbApplicationTests.java
@@ -1,6 +1,8 @@
 package com.mysite.sbb;
 
-import java.time.LocalDateTime;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.util.List;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,19 +16,27 @@ class SbbApplicationTests {
 
 	@Test
 	void testJpa() {
-		// 질문 데이터 저장
-		Question q1 = new Question();
-		q1.setSubject("sbb가 무엇인가요?");
-		q1.setContent("sbb에 대해 알고싶어요.");
-		q1.setCreateDate(LocalDateTime.now());
-		// 첫번째 질문 저장
-		this.questionRepository.save(q1);
 
-		Question q2 = new Question();
-		q2.setSubject("스프링부트 모델 질문입니다.");
-		q2.setContent("id는 자동으로 생성되나요?");
-		q2.setCreateDate(LocalDateTime.now());
-		// 두번째 질문 저장
-		this.questionRepository.save(q2);
+//		// 질문 데이터 저장
+//		Question q1 = new Question();
+//		q1.setSubject("sbb가 무엇인가요?");
+//		q1.setContent("sbb에 대해 알고싶어요.");
+//		q1.setCreateDate(LocalDateTime.now());
+//		// 첫번째 질문 저장
+//		this.questionRepository.save(q1);
+//
+//		Question q2 = new Question();
+//		q2.setSubject("스프링부트 모델 질문입니다.");
+//		q2.setContent("id는 자동으로 생성되나요?");
+//		q2.setCreateDate(LocalDateTime.now());
+//		// 두번째 질문 저장
+//		this.questionRepository.save(q2);
+
+		// findAll
+		List<Question> all = this.questionRepository.findAll();
+		assertEquals(2, all.size());
+
+		Question q = all.get(0);
+		assertEquals("sbb가 무엇인가요?", q.getSubject());
 	}
 }
