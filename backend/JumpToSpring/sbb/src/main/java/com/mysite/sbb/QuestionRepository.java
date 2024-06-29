9b78935d776058682ed63ca2379bc729b6dffcea
danmiee <dan-mi@kakao.com>
2024-06-29 17:07:43 +0900

    Test for findBySubjectAndContent
    - create vars

--- backend/JumpToSpring/sbb/src/main/java/com/mysite/sbb/QuestionRepository.java
+++ backend/JumpToSpring/sbb/src/main/java/com/mysite/sbb/QuestionRepository.java
@@ -4,4 +4,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface QuestionRepository extends JpaRepository<Question, Integer> {
 	Question findBySubject(String subject);
+
+	Question findBySubjectAndContent(String subject, String content);
 }
