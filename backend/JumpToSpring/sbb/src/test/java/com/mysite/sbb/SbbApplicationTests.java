2522f09b717522bd0780949bd5d06a15ced8eeaa
danmiee <dan-mi@kakao.com>
2024-06-29 16:31:35 +0900

    Test for save QUESTION table

--- backend/JumpToSpring/sbb/src/test/java/com/mysite/sbb/SbbApplicationTests.java
+++ backend/JumpToSpring/sbb/src/test/java/com/mysite/sbb/SbbApplicationTests.java
@@ -1,13 +1,32 @@
 package com.mysite.sbb;
 
+import java.time.LocalDateTime;
+
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class SbbApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+	@Autowired
+	private QuestionRepository questionRepository;
 
+	@Test
+	void testJpa() {
+		// 질문 데이터 저장
+		Question q1 = new Question();
+		q1.setSubject("sbb가 무엇인가요?");
+		q1.setContent("sbb에 대해 알고싶어요.");
+		q1.setCreateDate(LocalDateTime.now());
+		// 첫번째 질문 저장
+		this.questionRepository.save(q1);
+
+		Question q2 = new Question();
+		q2.setSubject("스프링부트 모델 질문입니다.");
+		q2.setContent("id는 자동으로 생성되나요?");
+		q2.setCreateDate(LocalDateTime.now());
+		// 두번째 질문 저장
+		this.questionRepository.save(q2);
+	}
 }
