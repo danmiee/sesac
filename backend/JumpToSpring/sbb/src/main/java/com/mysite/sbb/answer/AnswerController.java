5465e386e11f92b62483620e880237c867dcc9fb
danmiee <danmiee@github.com>
2024-06-30 15:38:55 +0900

    Create answer service and use in controller

--- backend/JumpToSpring/sbb/src/main/java/com/mysite/sbb/answer/AnswerController.java
+++ backend/JumpToSpring/sbb/src/main/java/com/mysite/sbb/answer/AnswerController.java
@@ -3,6 +3,7 @@ package com.mysite.sbb.answer;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -17,11 +18,14 @@ import lombok.RequiredArgsConstructor;
 public class AnswerController {
 
 	private final QuestionService questionService;
+	private final AnswerService answerService;
 
+	@PostMapping("/create/{id}")
 	public String createAnswer(Model model, @PathVariable("id") Integer id,
 			@RequestParam(value = "content") String content) {
 		Question question = this.questionService.getQuestion(id);
 		// 답변 저장
-		return String.format("redirect:/question/detael/%s", id);
+		this.answerService.create(question, content);
+		return String.format("redirect:/question/detail/%s", id);
 	}
 }
---
+++ backend/JumpToSpring/sbb/src/main/java/com/mysite/sbb/answer/AnswerService.java
@@ -0,0 +1,25 @@
+package com.mysite.sbb.answer;
+
+import java.time.LocalDateTime;
+
+import org.springframework.stereotype.Service;
+
+import com.mysite.sbb.question.Question;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class AnswerService {
+
+	private final AnswerRepository answerRepository;
+
+	public void create(Question question, String content) {
+		Answer answer = new Answer();
+		answer.setContent(content);
+		answer.setCreateDate(LocalDateTime.now());
+		answer.setQuestion(question);
+		this.answerRepository.save(answer);
+	}
+
+}
