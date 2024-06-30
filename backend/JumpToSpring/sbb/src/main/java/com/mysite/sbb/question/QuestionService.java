2cf13848cb7cf54715be1836f6c52f9bd3cb5611
danmiee <danmiee@github.com>
2024-06-30 17:20:10 +0900

    Implemented question registration

--- backend/JumpToSpring/sbb/src/main/java/com/mysite/sbb/question/QuestionService.java
+++ backend/JumpToSpring/sbb/src/main/java/com/mysite/sbb/question/QuestionService.java
@@ -1,5 +1,6 @@
 package com.mysite.sbb.question;
 
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
 
@@ -30,5 +31,13 @@ public class QuestionService {
 			// DataNotFoundException 클래스 필요
 			throw new DataNotFoundException("question not found");
 		}
+	}
+
+	public void create(String subject, String content) {
+		Question q = new Question();
+		q.setSubject(subject);
+		q.setContent(content);
+		q.setCreateDate(LocalDateTime.now());
+		this.questionRepository.save(q);
 	}
 }
