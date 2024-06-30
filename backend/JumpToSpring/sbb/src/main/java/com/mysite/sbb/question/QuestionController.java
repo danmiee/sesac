d56a64415993f8495bcfad944c861fb32e928c1a
danmiee <danmiee@github.com>
2024-06-30 14:14:06 +0900

    Passing data to template

--- backend/JumpToSpring/sbb/src/main/java/com/mysite/sbb/question/QuestionController.java
+++ backend/JumpToSpring/sbb/src/main/java/com/mysite/sbb/question/QuestionController.java
@@ -1,14 +1,25 @@
 package com.mysite.sbb.question;
 
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import lombok.RequiredArgsConstructor;
+
 @Controller
+@RequiredArgsConstructor
 public class QuestionController {
+
+	private final QuestionRepository questionRepository;
 
 	@GetMapping("/question/list")
 //	@ResponseBody	// 템플릿 사용 시 불필요
-	public String list() {
+	public String list(Model model) {
+
+		List<Question> questionList = this.questionRepository.findAll();
+		model.addAttribute("questionList", questionList);
 		return "question_list"; // question_list.html 파일 호출
 	}
 }
