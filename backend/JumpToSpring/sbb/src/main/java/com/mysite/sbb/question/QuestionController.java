616f616666e1af1054cb8d18c4fa5cb2d1d827f3
danmiee <dan-mi@kakao.com>
2024-06-30 15:22:09 +0900

    Use url prefix

--- backend/JumpToSpring/sbb/src/main/java/com/mysite/sbb/question/QuestionController.java
+++ backend/JumpToSpring/sbb/src/main/java/com/mysite/sbb/question/QuestionController.java
@@ -6,17 +6,19 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import lombok.RequiredArgsConstructor;
 
 @Controller
 @RequiredArgsConstructor // final 속성 포함하는 생성자 자동 완성
+@RequestMapping("/question") // url 프리픽스 사용
 public class QuestionController {
 
 //	private final QuestionRepository questionRepository;
 	private final QuestionService questionService;
 
-	@GetMapping("/question/list")
+	@GetMapping("/list")
 //	@ResponseBody	// 템플릿 사용 시 불필요
 	public String list(Model model) {
 
@@ -27,7 +29,7 @@ public class QuestionController {
 		return "question_list"; // question_list.html 파일 호출
 	}
 
-	@GetMapping("/question/detail/{id}")
+	@GetMapping("/detail/{id}")
 	public String detail(Model model, @PathVariable("id") Integer id) {
 		Question question = this.questionService.getQuestion(id);
 		model.addAttribute("question", question);
