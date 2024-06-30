c02a88449908426681042e3260c834c76a1e245c
danmiee <dan-mi@kakao.com>
2024-06-30 14:39:05 +0900

    Use Root URL

--- backend/JumpToSpring/sbb/src/main/java/com/mysite/sbb/MainController.java
+++ backend/JumpToSpring/sbb/src/main/java/com/mysite/sbb/MainController.java
@@ -17,4 +17,10 @@ public class MainController {
 	public String index() {
 		return "안녕하세요 sbb에 오신 것을 환영합니다.";
 	}
+
+	@GetMapping("/") // url 매핑
+	public String root() {
+		// 지정한 페이지로 리다이렉트(클라이언트가 요청하면 새로운 url로 전송)
+		return "redirect:/question/list";
+	}
 }
