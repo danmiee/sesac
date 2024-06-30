64b6ec2c85674f7b9717aae3dd939ac89153ada8
danmiee <danmiee@github.com>
2024-06-30 16:41:08 +0900

    Map url

--- backend/JumpToSpring/sbb/src/main/java/com/mysite/sbb/question/QuestionController.java
+++ backend/JumpToSpring/sbb/src/main/java/com/mysite/sbb/question/QuestionController.java
@@ -6,6 +6,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import lombok.RequiredArgsConstructor;
@@ -34,5 +35,10 @@ public class QuestionController {
 		Question question = this.questionService.getQuestion(id);
 		model.addAttribute("question", question);
 		return "question_detail";
+	}
+
+	@PostMapping("/create")
+	public String questionCreate() {
+		return "question_form";
 	}
 }
