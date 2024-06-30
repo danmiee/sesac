2ef7a397145e6b1cb3379569fbc078e681e19ce4
danmiee <dan-mi@kakao.com>
2024-06-30 15:12:50 +0900

    Display detail page on the screen by service object

--- backend/JumpToSpring/sbb/src/main/java/com/mysite/sbb/question/QuestionService.java
+++ backend/JumpToSpring/sbb/src/main/java/com/mysite/sbb/question/QuestionService.java
@@ -1,8 +1,11 @@
 package com.mysite.sbb.question;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.stereotype.Service;
+
+import com.mysite.sbb.DataNotFoundException;
 
 import lombok.RequiredArgsConstructor;
 
@@ -17,5 +20,15 @@ public class QuestionService {
 
 	public List<Question> getList() {
 		return this.questionRepository.findAll();
+	}
+
+	public Question getQuestion(Integer id) {
+		Optional<Question> question = this.questionRepository.findById(id);
+		if (question.isPresent()) {
+			return question.get();
+		} else {
+			// DataNotFoundException 클래스 필요
+			throw new DataNotFoundException("question not found");
+		}
 	}
 }
