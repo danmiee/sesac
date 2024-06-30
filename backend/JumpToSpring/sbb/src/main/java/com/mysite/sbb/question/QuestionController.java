b7153f0654353eebebee6e4a34e3757070c8a32d
danmiee <dan-mi@kakao.com>
2024-06-30 17:31:12 +0900

    Send questionForm to controller

--- backend/JumpToSpring/sbb/src/main/java/com/mysite/sbb/question/QuestionController.java
+++ backend/JumpToSpring/sbb/src/main/java/com/mysite/sbb/question/QuestionController.java
@@ -4,12 +4,13 @@ import java.util.List;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 
 @Controller
@@ -43,11 +44,24 @@ public class QuestionController {
 		return "question_form";
 	}
 
+//	@PostMapping("/create")
+//	public String questionCreate(@RequestParam(value = "subject") String subject,
+//			@RequestParam(value = "content") String content) {
+//		// 질문 저장
+//		this.questionService.create(subject, content);
+//		return "redirect:/question/list"; // 질문 저장 후 질문 목록으로 이동
+//	}
+
+	// form 클래스 받기
 	@PostMapping("/create")
-	public String questionCreate(@RequestParam(value = "subject") String subject,
-			@RequestParam(value = "content") String content) {
-		// 질문 저장
-		this.questionService.create(subject, content);
-		return "redirect:/question/list"; // 질문 저장 후 질문 목록으로 이동
+	// 매개변수를 검증이 수행된 결과를 의미하는 객체로 변경
+	// valid 후 BindingResult 나와야 정상 반영됨
+	public String questionCreate(@Valid QuestionFrom questionFrom, BindingResult bindingResult) {
+		// 오류나면 질문 등록 화면으로 돌아가기
+		if (bindingResult.hasErrors()) {
+			return "question_form";
+		}
+		this.questionService.create(questionFrom.getSubject(), questionFrom.getContent());
+		return "redirect:/question/list";
 	}
 }
