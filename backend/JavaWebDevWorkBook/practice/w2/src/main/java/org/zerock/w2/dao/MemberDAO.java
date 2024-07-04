12148ae0614810ed01dbe75f7aa2753381ef4c6c
danmiee <danmiee@github.com>
2024-07-04 14:33:10 +0900

    Feat: Processing member data

--- backend/JavaWebDevWorkBook/practice/w2/src/main/java/org/zerock/w2/dao/MemberDAO.java
+++ backend/JavaWebDevWorkBook/practice/w2/src/main/java/org/zerock/w2/dao/MemberDAO.java
@@ -1,2 +1,35 @@
-package org.zerock.w2.dao;public class MemberDAO {
+package org.zerock.w2.dao;
+
+import lombok.Cleanup;
+import org.zerock.w2.domain.MemberVO;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+public class MemberDAO {
+
+    public MemberVO getWithPassword(String mid, String mpw) throws  Exception {
+        String query = "select mid, mpw, mname from tbl_member where mimd = ? and mpw = ?";
+
+        MemberVO memberVO = null;
+
+        @Cleanup Connection connection = ConnectionUtil.INSTANCE.getConnection();
+        @Cleanup PreparedStatement preparedStatement = connection.prepareStatement(query);
+
+        preparedStatement.setString(1, mid);
+        preparedStatement.setString(2, mpw);
+
+        @Cleanup ResultSet resultSet = preparedStatement.executeQuery();
+
+        resultSet.next();
+
+        memberVO = MemberVO.builder()
+                .mid(resultSet.getString(1))
+                .mpw(resultSet.getString(2))
+                .mname(resultSet.getString(3))
+                .build();
+
+        return memberVO;
+    }
 }
--- backend/JavaWebDevWorkBook/practice/w2/src/main/java/org/zerock/w2/domain/MemberVO.java
+++ backend/JavaWebDevWorkBook/practice/w2/src/main/java/org/zerock/w2/domain/MemberVO.java
@@ -1,2 +1,14 @@
-package org.zerock.w2.domain;public class MemberVO {
+package org.zerock.w2.domain;
+
+import lombok.*;
+
+@Getter
+@ToString
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class MemberVO {
+    private String mid;
+    private String mpw;
+    private String mname;
 }
