09f8e49fd838152c96263445081e6b88c588002f
danmiee <dan-mi@kakao.com>
2024-07-04 13:31:28 +0900

    Feat: Implement LoginCheckFilter

--- backend/JavaWebDevWorkBook/practice/w2/src/main/java/org/zerock/w2/filter/LoginCheckFilter.java
+++ backend/JavaWebDevWorkBook/practice/w2/src/main/java/org/zerock/w2/filter/LoginCheckFilter.java
@@ -4,6 +4,9 @@ import lombok.extern.log4j.Log4j2;
 
 import javax.servlet.*;
 import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
 
 @WebFilter(urlPatterns = {"/todo/*"})
@@ -12,6 +15,16 @@ public class LoginCheckFilter implements Filter {
     @Override
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
         log.info("Login check filter....");
+
+        HttpServletRequest req = (HttpServletRequest)request;
+        HttpServletResponse resp = (HttpServletResponse)response;
+
+        HttpSession session = req.getSession();
+
+        if(session.getAttribute("loginInfo") == null) {
+            resp.sendRedirect("/login");
+            return;
+        }
         chain.doFilter(request, response);
     }
 }
---
+++ backend/JavaWebDevWorkBook/practice/w2/src/main/java/org/zerock/w2/filter/UTF8Filter.java
@@ -0,0 +1,2 @@
+package org.zerock.w2.filter;public class UTF8Filter {
+}
