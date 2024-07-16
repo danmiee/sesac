6caf68d2ed063de336969b4e1d5da9d44e18b5d3
danmiee <danmiee@github.com>
2024-07-16 16:41:01 +0900

    json parsing and test

--- backend/JavaWebDevWorkBook/practice/api01/src/main/java/org/zerock/api01/security/filter/APILoginFilter.java
+++ backend/JavaWebDevWorkBook/practice/api01/src/main/java/org/zerock/api01/security/filter/APILoginFilter.java
@@ -1,11 +1,16 @@
 package org.zerock.api01.security.filter;
 
+import com.google.gson.Gson;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.authentication.AbstractAuthenticationProcessingFilter;
+
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.util.Map;
 
 @Log4j2
 public class APILoginFilter extends AbstractAuthenticationProcessingFilter {
@@ -16,6 +21,26 @@ public class APILoginFilter extends AbstractAuthenticationProcessingFilter {
     @Override
     public Authentication attemptAuthentication(HttpServletRequest request, HttpServletResponse response) throws AuthenticationException, IndexOutOfBoundsException {
         log.info("APILoginFilter-------------------");
+
+        if (request.getMethod().equalsIgnoreCase("GET")) {
+            log.info("GET METHOD NOT SUPPORT");
+            return null;
+        }
+
+        Map<String, String> jsonData = parseRequestJSON(request);
+        log.info("jsonData: " + jsonData);
+        return null;
+    }
+
+    private Map<String, String> parseRequestJSON(HttpServletRequest request) {
+
+        // JSON 데이터 분석하여 mid, mpw 전달 값 Map으로 처리
+        try (Reader reader = new InputStreamReader(request.getInputStream())) {
+            Gson gson = new Gson();
+            return gson.fromJson(reader, Map.class);
+        } catch (Exception e) {
+            log.error(e.getMessage());
+        }
         return null;
     }
 }
