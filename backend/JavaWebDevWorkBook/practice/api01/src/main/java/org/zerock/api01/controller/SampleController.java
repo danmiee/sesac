36770daa08166b973b99609ce90b4c9f02e4e215
danmiee <danmiee@github.com>
2024-07-16 15:46:31 +0900

    Operation 옵션 추가

--- backend/JavaWebDevWorkBook/practice/api01/src/main/java/org/zerock/api01/controller/SampleController.java
+++ backend/JavaWebDevWorkBook/practice/api01/src/main/java/org/zerock/api01/controller/SampleController.java
@@ -11,7 +11,8 @@ import java.util.List;
 @RestController
 @RequestMapping("/api/sample")
 public class SampleController {
-    @Operation(summary = "Sample GET doA")
+//    summary와 description은 Swagger UI에서 API 문서를 확인할 때 사용되는 정보이다. 노출되는 위치가 다름.
+    @Operation(summary = "doA method", description = "샘플 doA 가져오기")
     @GetMapping("/doA")
     public List<String> doA() {
         return Arrays.asList("AAA", "BBB", "CCC");
