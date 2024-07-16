2d11b7e018876284440a179204ed39cb40d79f14
danmiee <danmiee@github.com>
2024-07-16 15:38:15 +0900

    Create CustomServletConfig

---
+++ backend/JavaWebDevWorkBook/practice/api01/src/main/java/org/zerock/api01/config/CustomServletConfig.java
@@ -0,0 +1,10 @@
+package org.zerock.api01.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebMvc
+public class CustomServletConfig implements WebMvcConfigurer {
+}
